c6b402d73014753b1aa69ed0fd0dd966a966b62c
akarzazi <akarzazi@gmail.com>
2020-03-29 16:52:55 +0200

    Add nlog file logging

--- src/UdpLink.Server/Program.cs
+++ src/UdpLink.Server/Program.cs
@@ -1,10 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Config;
+using NLog.Extensions.Logging;
 using UdpLink.Server.Command;
 using UdpLink.Server.Config;
 using UdpLink.Shared.Helpers;
@@ -15,38 +21,55 @@ namespace UdpLink.Server
     {
         public static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            var baseDir = ProgramUtils.GetBaseDir();
+            Directory.SetCurrentDirectory(baseDir);
+
             var config = new ConfigurationBuilder()
                     .SetBasePath(ProgramUtils.GetBaseDir())
                     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                     .Build();
-            var listenerConfig = config.GetSection("ListenerConfig").Get<ListenerConfig>();
 
+            ConfigureLogging(config);
+
+            var listenerConfig = config.GetSection("ListenerConfig").Get<ListenerConfig>();
             CreateHostBuilder(args, listenerConfig).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args, ListenerConfig config)
         {
+
+
             return Host.CreateDefaultBuilder(args)
                  .UseWindowsService()
                 .ConfigureServices((hostContext, services) =>
                 {
-                    services.AddTransient<ListenerConfig>((s) => config);                
+                    services.AddTransient<ListenerConfig>((s) => config);
                     services.AddHostedService<UdpListenerService>();
                     RegisterCmdHandlers(services);
 
+                }).ConfigureLogging((logging) =>
+                {
+                    logging.ClearProviders();
+                    logging.AddNLog();
                 });
-
         }
 
         private static void RegisterCmdHandlers(IServiceCollection services)
         {
             services.AddTransient<CommandExecutorService>();
-            services.AddTransient<EchoCommandHandler>();
-            services.AddTransient<PowershellCommandHandler>();
-            services.AddTransient<RebootCommandHandler>(); 
             services.AddTransient<ICommandHandler, EchoCommandHandler>();
             services.AddTransient<ICommandHandler, PowershellCommandHandler>();
             services.AddTransient<ICommandHandler, RebootCommandHandler>();
+        }
+
+        private static void ConfigureLogging(IConfigurationRoot config)
+        {
+            var path = config.GetSection("Logging:LogPath").Value;
+            if (path == null)
+                throw new Exception("Missing configuration section 'Logging:LogPath', ensure the entry is in appsettings.json file");
+            GlobalDiagnosticsContext.Set("LogPath", path);
         }
     }
 }
